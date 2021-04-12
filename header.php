b354b18fc4a671fb98e56a782637c60010170168
ananditspecial <82330692+ananditspecial@users.noreply.github.com>
2021-04-12 12:30:52 +0530

    asdf
    asdf

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="keywords" content="advanced custom search, agency, agent, business, clean, corporate, directory, google maps, homes, idx agent, listing properties, membership packages, property, real broker, real estate, real estate agent, real estate agency, realtor">
-<meta name="description" content="FindHouse - Real Estate HTML Template">
+<meta name="description" content="Realty Sphere - Real Estate website">
 <meta name="CreativeLayers" content="ATFN">
 <!-- css file -->
 <link rel="stylesheet" href="css/bootstrap.min.css">
@@ -14,7 +14,7 @@
 <!-- Responsive stylesheet -->
 <link rel="stylesheet" href="css/responsive.css">
 <!-- Title -->
-<title>FindHouse - Real Estate HTML Template</title>
+<title>Realty Sphere - Real Estate Website</title>
 <!-- Favicon -->
 <link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
 <link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" />
@@ -26,7 +26,7 @@
 
 	<!-- Main Header Nav -->
 	<header class="header-nav menu_style_home_one navbar-scrolltofixed stricky main-menu">
-		<div class="container-fluid p0">
+		<div class="container p0">
 		    <!-- Ace Responsive Menu -->
 		    <nav>
 		        <!-- Menu Toggle btn-->
@@ -38,54 +38,74 @@
 		                <span class="icon-bar"></span>
 		            </button>
 		        </div>
-		        <a href="#" class="navbar_brand float-left dn-smd">
+		        <a href="index.php" class="navbar_brand float-left dn-smd">
+
 		            <img class="logo1 img-fluid" src="images/header-logo.png" alt="header-logo.png">
 		            <img class="logo2 img-fluid" src="images/header-logo2.png" alt="header-logo2.png">
-		            <span>FindHouse</span>
 		        </a>
 		        <!-- Responsive Menu Structure-->
 		        <!--Note: declare the Menu style in the data-menu-style="horizontal" (options: horizontal, vertical, accordion) -->
-		        <ul id="respMenu" class="ace-responsive-menu text-right" data-menu-style="horizontal">
+		       
+				<ul id="respMenu" class="ace-responsive-menu text-right" data-menu-style="horizontal">
 		            <li>
-		                <a href="index.html"><span class="title">Home</span></a>
+		                <div class="ui_kit_input top-search">
+							<form class="footer_mailchimp_form top-search">
+								<div class="form-row align-items-center">
+								   <div class="col-auto">
+									   <input type="email" class="form-control mb-2" id="inlineFormInput" placeholder="Enter Your Keyword...">
+								   </div>
+								   <div class="col-auto">
+									   <button type="submit" class="btn btn-primary mb-2"><i class="flaticon-magnifying-glass"></i></button>
+								   </div>
+								 </div>
+						   </form>
+						</div>
+		            </li>
+					<li>
+		                <a href="index.php"><span class="title">Home</span></a>
 		            </li>
 		            <li>
-		                <a href="page-listing-grid-v1.html"><span class="title">Listing</span></a>
+		                <a href="#"><span class="title">Listing</span></a>
 		                <!-- Level Two-->
 	                	<ul>
 		                    <li>
-		                        <a href="page-listing-parallax.html">Listing Style</a>
+		                        <a href="property-list.php">Listing Style</a>
 		                        <!-- Level Three-->
 		                    </li>
-		                    <li>
-		                        <a href="page-listing-agencies-v1.html">Agencies View</a>
+                            <li>
+		                        <a href="property-single.php">Property Single</a>
 		                    </li>
+		                    <li>
+		                        <a href="agency.php">Agencies View</a>
+		                    </li>
+							
 	                	</ul>
 		            </li>
-		            <li>
-		                <a href="page-listing-single-v1.html"><span class="title">Property Single</span></a>
-		            </li>
+		            <!-- <li>
+		                <a href="page-listing-single-v1.php"><span class="title">Property Single</span></a>
+		            </li> -->
 		            <li>
 		                <a href="#"><span class="title">Pages</span></a>
 		                <ul>
-		                    <li><a href="page-about.html">About Us</a></li>
-		                    <li><a href="page-gallery.html">Gallery</a></li>
-		                    <li><a href="page-faq.html">Faq</a></li>
-		                    <li><a href="page-service.html">Service</a></li>
-		                    <li><a href="page-error.html">404 Page</a></li>
-		                    <li><a href="page-terms.html">Terms and Conditions</a></li>
+		                    <li><a href="about.php">About Us</a></li>
+		                    <li><a href="gallery.php">Gallery</a></li>
+		                    <li><a href="faq.php">Faq</a></li>
+		                    <li><a href="service.php">Service</a></li>
+		                    <li><a href="error.php">404 Page</a></li>
+		                    <li><a href="terms.php">Terms and Conditions</a></li>
 		                </ul>
 		            </li>
 		            <li>
 		                <a href="#"><span class="title">Blog</span></a>
 		                <ul>
-		                    <li><a href="page-blog-v1.html">Blog List 1</a></li>
-		                    <li><a href="page-blog-grid.html">Blog List 2</a></li>
-		                    <li><a href="page-blog-single.html">Single Post</a></li>
+		                    <li><a href="blog.php">Blog List</a></li>
+		                    <li><a href="blog-single.php">Single Post</a></li>
 		                </ul>
 		            </li>
+					<li>
+		                <a href="contact.php"><span class="title">Contact Us</span></a>
+		            </li>
 		            
-	                <li class="list-inline-item add_listing last"><a href="page-contact.html"><span class="flaticon-plus"></span><span class="dn-lg"> Contact Us</span></a></li>
 		        </ul>
 		    </nav>
 		</div>
@@ -93,57 +113,66 @@
 
 	<!-- Main Header Nav For Mobile -->
 	<div id="page" class="stylehome1 h0">
-		<div class="mobile-menu">
+	<div class="mobile-menu">
 			<div class="header stylehome1">
 				<div class="main_logo_home2 text-center">
 		            <img class="nav_logo_img img-fluid mt20" src="images/header-logo2.png" alt="header-logo2.png">
-		            <span class="mt20">FindHouse</span>
 				</div>
 				<ul class="menu_bar_home2">
-	                <li class="list-inline-item list_s"><a href="page-register.html"><span class="flaticon-user"></span></a></li>
+	                <li class="list-inline-item list_s phone"><a href="tel:9315244186"><span class="fa fa-phone"></span></a></li>
 					<li class="list-inline-item"><a href="#menu"><span></span></a></li>
 				</ul>
 			</div>
 		</div><!-- /.mobile-menu -->
 		<nav id="menu" class="stylehome1">
 			<ul>
-				<li><a href="index.html">Home 1</a></li>
-				<li><span>Listing</span>
+				<li>
+					<form class="footer_mailchimp_form top-search mobile-search">
+						<div class="form-row align-items-center">
+						   <div class="col-auto">
+							   <input type="email" class="form-control mb-2" id="inlineFormInput" placeholder="Enter Your Keyword...">
+						   </div>
+						   <div class="col-auto">
+							   <button type="submit" class="btn btn-primary mb-2"><i class="flaticon-magnifying-glass"></i></button>
+						   </div>
+						 </div>
+				   </form>
+				</li>
+				<li><a href="index.php">Home </a></li>
+				<li>
+					<a href=""><span class="title">Listing</span></a>
+					<!-- Level Two-->
 					<ul>
-						    <li>
-		                <a href="page-listing-grid-v1.html"><span class="title">Listing</span></a>
-		                <!-- Level Two-->
-	                	<ul>
-		                    <li>
-		                        <a href="page-listing-parallax.html">Listing Style</a>
-		                        <!-- Level Three-->
-		                    </li>
-		                    <li>
-		                        <a href="page-listing-agencies-v1.html">Agencies View</a>
-		                    </li>
-	                	</ul>
-		            </li>
+						<li>
+							<a href="property-list.php">Listing Style</a>
+							<!-- Level Three-->
+						</li>
+						<li>
+							<a href="agency.php">Agencies View</a>
+						</li>
+						<li>
+							<a href="property-single.php">Property Single</a>
+						</li>
 					</ul>
 				</li>
 				<li><span>Blog</span>
 					<ul>
-	                    <li><a href="page-blog-v1.html">Blog List 1</a></li>
-	                    <li><a href="page-blog-grid.html">Blog List 2</a></li>
-	                    <li><a href="page-blog-single.html">Single Post</a></li>
+	                    <li><a href="blog.php">Blog List</a></li>
+	                    <li><a href="blog-single.php">Single Post</a></li>
 					</ul>
 				</li>
 				<li><span>Pages</span>
 					<ul>
 					
-		                <li><a href="page-about.html">About Us</a></li>
-		                <li><a href="page-gallery.html">Gallery</a></li>
-		                <li><a href="page-faq.html">Faq</a></li>
-	                    <li><a href="page-service.html">Service</a></li>
-		                <li><a href="page-error.html">404 Page</a></li>
-		                <li><a href="page-terms.html">Terms and Conditions</a></li>
+		                <li><a href="about.php">About Us</a></li>
+		                <li><a href="gallery.php">Gallery</a></li>
+		                <li><a href="faq.php">Faq</a></li>
+	                    <li><a href="service.php">Service</a></li>
+		                <li><a href="error.php">404 Page</a></li>
+		                <li><a href="terms.php">Terms and Conditions</a></li>
 					</ul>
 				</li>
-				<li class="cl_btn"><a class="btn btn-block btn-lg btn-thm circle" href="page-contact.html"><span class="flaticon-plus"></span> Contact Us</a></li>
+				<li class="cl_btn"><a class="btn btn-block btn-lg btn-thm circle" href="contact.php"><span class="flaticon-plus"></span> Contact Us</a></li>
 			</ul>
 		</nav>
 	</div>
